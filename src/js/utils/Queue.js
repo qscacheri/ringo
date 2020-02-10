2030511ddab617aa03f829a8c923165801400f45
Quin Scacheri <qs366@nyu.edu>
2020-02-10 11:21:32 -0500

    added queue class

---
+++ src/js/utils/Queue.js
@@ -0,0 +1,24 @@
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    enqueue(newItem) 
+    {
+        this.items.push(newItem);
+    }
+
+    dequeue()
+    {
+        if (this.isEmpty())
+            return "Underflow";
+        return this.items.shift();
+    }
+
+    isEmpty()
+    {
+        return (this.items.length > 0 ? false : true)  
+    }
+}
+
+export default Queue;
