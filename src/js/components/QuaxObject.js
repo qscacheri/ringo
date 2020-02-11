6b92aed6d30b47e5520867400d824b42ac1aaee8
Quin Scacheri <qs366@nyu.edu>
2020-02-11 13:31:05 -0500

    added double click functionality

--- src/js/components/QuaxObject.js
+++ src/js/components/QuaxObject.js
@@ -5,7 +5,7 @@ import '../../css/QuaxObject.css';
 import IOLet from "./IOLet";
 import { IOLetType } from './IOLet'
 import { OBJECT_TYPES } from "../constants/object-types.js";
-import { objectTypeChanged, sendObjectData, testThunk } from '../actions/actions'
+import { updateObject as updateObject, sendObjectData, selectNewObject } from '../actions/actions'
 
 function mapStateToProps(state) {
     return {
@@ -15,18 +15,20 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        objectTypeChanged: newType => dispatch(objectTypeChanged(newType)),
+        updateObject: newType => dispatch(updateObject(newType)),
         sendObjectData: data => dispatch(sendObjectData(data)),
-        testThunk: thunk => dispatch(testThunk())
+        selectNewObject: newObject => dispatch(selectNewObject(newObject))
     };
 }
 
 class ConnectedQuaxObject extends Component {
     constructor(props) {
         super(props);
+        this.ref = React.createRef();
         this.state = {
+            isDrag: false, 
             textValue: '',
-            inputDisabled: false
+            inputDisabled: true
         }
     }
 
@@ -34,45 +36,44 @@ class ConnectedQuaxObject extends Component {
         this.setState({ textValue: event.target.value });
     }
 
+    handleDrag(e, data)
+    {
+        console.log(data);
+        this.setState({isDrag: true})
+    }
+
+
     handleClick(e) {
-        console.log("Clicked on object with id:", this.props.id);
+        if (this.state.isDrag == false)
+        {
+            console.log("Clicked on object with id:", this.props.id);
+            this.setState({inputDisabled: false});
+            console.log(this.ref);
+            this.ref.current.focus();
+        }
         
+        this.props.selectNewObject({id: this.props.id});
         switch (this.props.type) {
             case OBJECT_TYPES.BUTTON:
-                console.log("button clicked, bang sent");
                 this.props.sendObjectData({value: 'BANG', outletIndex: 0, objectId: this.props.id})
                 break;
             default: break
         }
         e.stopPropagation();
+        this.setState({isDrag: false});
     }
 
-    handleSubmit(e) {
+    handleSubmit(e) 
+    {
         e.preventDefault();
         var newObjectType = this.state.textValue.toUpperCase();
-        for (var type in OBJECT_TYPES) {
-            if (OBJECT_TYPES[type] === newObjectType) {
-                this.props.objectTypeChanged({ id: parseInt(this.props.id), newObjectType: newObjectType });
-                switch (newObjectType) {
-                    case OBJECT_TYPES.BUTTON:
-                        this.setState({inputDisabled: true, textValue: ""})
-                        break;
-                    default: break
-                }
-                return;
-            }
-        }
-
-        this.props.objectTypeChanged({ id: parseInt(this.props.id), newObjectType: OBJECT_TYPES.INVALID });
-
+        this.props.updateObject({id: parseInt(this.props.id), objectText: this.state.textValue});
+        return;
     }
 
-    render() {
-
-
+    render() {        
         var inlets = [];
         var outlets = [];
-
 
         for (let i = 0; i < this.props.numInlets; i++) {
             inlets.push(<IOLet key={this.props.id + ":" + IOLetType.In + "" + i} ioletIndex={i} parentId={this.props.id} connectionType={IOLetType.In}></IOLet>)
@@ -89,13 +90,13 @@ class ConnectedQuaxObject extends Component {
             className = 'QuaxObject'
 
         return (
-            <Draggable enableUserSelectHack={false} defaultPosition={{ x: this.props.position.x, y: this.props.position.y }}>
+            <Draggable onDrag={this.handleDrag.bind(this)} enableUserSelectHack={false} defaultPosition={{ x: this.props.position.x, y: this.props.position.y }}>
                 <div className={className} onClick={this.handleClick.bind(this)}>
                     <div className="Inlets">
                         {inlets}
                     </div>
                     <form onSubmit={this.handleSubmit.bind(this)}>
-                        <input disabled={this.state.inputDisabled} onKeyDown={e => e.stopPropagation()} name='type' value={this.state.textValue} type="text" onChange={this.handleChange.bind(this)}></input>
+                        <input ref={this.ref} autoComplete="off" onBlur={ disableInput=>{ this.setState({inputDisabled: true})} }disabled={this.state.inputDisabled} onKeyDown={e => e.stopPropagation()} name='type' value={this.state.textValue} type="text" onChange={this.handleChange.bind(this)}></input>
                     </form>
                     <div className="Outlets">
                         {outlets}
