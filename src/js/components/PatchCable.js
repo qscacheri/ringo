46d272c533c8969a665452f8383a2176f43ad62e
Quin Scacheri <qs366@nyu.edu>
2020-05-03 14:24:59 -0400

    switched patch cable to functional component

--- src/js/components/PatchCable.js
+++ src/js/components/PatchCable.js
@@ -1,32 +1,27 @@
 /* eslint-disable */
 
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class PatchCable extends Component {
-    constructor(props)
-    {
-        super(props);
+function PatchCable(props) {
+
+    const handleClick = () => {
+        console.log('clicked on patch cable');
+        
     }
 
-    handleClick(e)
-    {
+    var strokeWidth = 2;
+    var left = 0;
+    var top = 0;
+    var width = '1000px';
+    var height = '1000px';
 
-    }
+    return (
+        <svg style={{ position: "absolute", width, height, left, top, pointerEvents: "none" }} className="PatchCable">
+            <line onClick={handleClick} x1={props.pos1.x} y1={props.pos1.y} x2={props.pos2.x} y2={props.pos2.y} style={{ display: "inline", strokeWidth, stroke: "aqua" }}></line>
+        </svg>
+    );
 
-    render() {                
-        var strokeWidth = 2;
-        var left = 0;
-        var top = 0;
-        var width = '1000px';
-        var height = '1000px';
-
-      return (
-          <svg style={{position: "absolute", width, height, left, top, pointerEvents: "none"}} className = "PatchCable">
-              <line onClick = {this.handleClick.bind(this)} x1={this.props.pos1.x} y1={this.props.pos1.y} x2={this.props.pos2.x} y2={this.props.pos2.y} style={{display: "inline", strokeWidth, stroke:"aqua"}}></line>
-          </svg>
-      );
-    }
 
 }
 
